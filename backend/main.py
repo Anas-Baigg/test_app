e82f20f331d051774ac71cb1be1142001e22fb32
Anas baig <anasmirza748@gmail.com>
2026-06-19 19:54:38 +0100

    fixed for docker

--- backend/main.py
+++ backend/main.py
@@ -44,5 +44,5 @@ def chat(request: MessageRequest):
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 7860))
     uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=False)
